fccdf40a70f021addb4d3def456a96bca518568d
Steve <stephen@savantdegrees.com>
2021-02-18 19:41:36 +0700

    add update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,24 @@
+msg=
+buffer=()
 version=
 
-git pull origin main
-git pull origin develop
+if [ -f update_processing.sh ]
+then
+	rm update_processing.sh
+fi
 
-echo -n "Enter version number:"
+echo "Current NSB version:"
+cat js/version.js
+
+echo -n "Enter version number: "
 read version
+echo "let nsbVersion = $version;" > js/version.js
 
-echo version
+#echo "git add js/version.js" >> update_processing.sh
+echo "git commit -a -m 'Bumped version to v$version'" >> update_processing.sh
+echo "git tag -a v$version -m 'Bumped version to v$version'" >> update_processing.sh
+echo "git push" >> update_processing.sh
+
+chmod +x update_processing.sh
+./update_processing.sh
 
